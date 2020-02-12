11c2af539a87798506df69d30349f20d73f9aeea
Jakeokony1024 <jakeokony@gmail.com>
2020-02-12 14:14:23 -0600

    installed all packages

--- liri.js
+++ liri.js
@@ -1,5 +1,22 @@
+var axios = require("axios");
 
+var fs = require("fs");
 
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 
 var spotify = new Spotify(keys.spotify);
+
+var spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
