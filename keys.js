fb8a70f4d10072e522d71d8b095324b5185a9e2e
Jakeokony1024 <jakeokony@gmail.com>
2020-02-12 18:43:39 -0600

    bands in town function works

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+exports.spotifyKeys = {
+  id: "17b4b7b533294f01be97a28aebaf87ae",
+  secret: "c058ca5c5f9a4cfbaf54e97cdc2543b1"
 };
--- liri.js
+++ liri.js
@@ -1,22 +1,73 @@
+var keys = require("./keys.js");
+
 var axios = require("axios");
 
-var fs = require("fs");
+var Spotify = require('node-spotify-api');
 
-var dotenv = require("dotenv").config();
+var moment = require('moment');
 
-var spotify = new Spotify(keys.spotify);
+var getMeShows = function(bandName){
 
-var spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
+
+axios.get("https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp").then(
+    function (response) {
+        
+        
+
+        for (var i = 0; i < response.data.length; i++){
+            response.data[i].datetime = moment(response.data[i].datetime).format("MM/DD/YYYY")
+            console.log(response.data[i].venue.name);
+            console.log(response.data[i].venue.city);
+            console.log(response.data[i].datetime);
+            console.log(response.data[i].lineup);
+            console.log("-----------------------------------")
+        }
+    })
+}
+
+// var spotify = new Spotify({
+//     id: keys.spotifyKeys.id,
+//     secret: keys.spotifyKeys.secret
+// });
+
+// var getMeArtists = function(artists){
+//     return artists.name;
+// }
+
+// var getMeSpotify = function (songName) {
+//     spotify.search({
+//         type: 'track',
+//         query: "all the small things"
+//     }, function (err, data) {
+//         if (err) {
+//             return console.log('Error occurred: ' + err);
+//         }
+//         console.log(data.tracks.items[0])
+//         var songs = data.tracks.items;
+//         for (var i = 0; i < songs.length; i++){
+//             console.log(i)
+//             console.log("artist(s): " + data.songs[i].artists.map(getMeArtists));
+//             console.log("song name: " + data.songs[i].name);
+//             console.log("preview song: " + data.songs[i].external_urls.spotify);
+//             console.log("album: " + data.songs[i].album.name);
+//             console.log("-----------------------------------------------------");
+//         }
+//     });
+// }
+
+var pick = function (caseData, functionData) {
+    switch (caseData) {
+        case "concert-this":
+            getMeShows(functionData);
+            break;
+        default:
+            console.log("LIRI doesn't know that.")
+    }
+}
+
+var runApp = function(argOne, Argtwo){
+    pick(argOne, Argtwo);
+}
+
+runApp(process.argv[2], process.argv[3]);
+
